955203be426f1dbea5fc5f74ab3c3ef88f7f908a
Zekeriya <Zekeriya@ZEKERIYA>
2024-07-31 20:28:30 +0300

    Add project files.

---
+++ PratikOkulYönetimSistemi/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratikOkulYönetimSistemi
+{
+    public class Person
+    {
+        string name;
+        string surname;
+        string birthDate;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+        public string BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+            set
+            {
+                birthDate = value;
+            }
+        }
+        public void Student()
+        {
+            Console.WriteLine($"Öğrencinin ismi: {name}, Öğrencinin Soyismi: {surname}, Öğrencinin doğum tarihi: {birthDate}");
+        }
+        public void Teacher()
+        {
+            Console.WriteLine($"Öğretmenin ismi: {name}, Öğretmenin Soyismi: {surname}, Öğretmenin doğum tarihi: {birthDate}");
+        }
+    }
+}
---
+++ PratikOkulYönetimSistemi/Program.cs
@@ -0,0 +1,29 @@
+﻿using PratikOkulYönetimSistemi;
+
+Person student = new Person();
+student.Name = "Harry";
+student.Surname = "Potter";
+student.BirthDate = "31 Temmuz 1980";
+
+Person student2 = new Person();
+student2.Name = "Hermione";
+student2.Surname = "Granger";
+student2.BirthDate = "19 Eylül 1979";
+
+Person student3 = new Person();
+student3.Name = "Ron";
+student3.Surname = "Weasley";
+student3.BirthDate = "1 Mart 1980";
+
+Person teacher = new Person();
+teacher.Name = "Albus";
+teacher.Surname = "Dumbledore";
+teacher.BirthDate = "Temmuz 1881";
+
+student.Student();
+student2.Student();
+student3.Student();
+Console.WriteLine("-----------------------------");
+teacher.Teacher();
+
+
